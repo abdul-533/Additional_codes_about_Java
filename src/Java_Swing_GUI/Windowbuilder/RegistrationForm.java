aec5c99ab727d83b2d10c71db37eb704847ce42b
Abdul <elizadeh9797@gmail.com>
2023-07-02 01:04:36 +0400

    next commit

--- src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
+++ src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
@@ -2,6 +2,9 @@ package Java_Swing_GUI.Windowbuilder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class RegistrationForm extends JDialog {
     private JTextField tfName;
@@ -23,12 +26,72 @@ public class RegistrationForm extends JDialog {
         setMinimumSize(new Dimension(700, 720));
         setModal(true);
         setLocationRelativeTo(parent);
-        setVisible(true);
 
+
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        setVisible(true);
+    }
+
+    private void registerUser() {
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        String address = tfAddress.getText();
+        String password = String.valueOf(pfPAssword.getPassword());
+        String confirmPassword = String.valueOf(pfPasswordConfirm.getPassword());
+
+        if (name.isEmpty() || email.isEmpty() || address.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Place enter all fields", "Try again", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        user = addUserToDataBase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        } else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to register new user",
+                    "Try again", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public User user;
+
+    private User addUserToDataBase(String name, String email, String phone, String address, String password) {
+        User user = null;
+        final String DB_Url = "jdbc:mysql://localhost/MyStore?ServerTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+        try {
+            Connection connect = DriverManager.getConnection(DB_Url, USERNAME, PASSWORD);
+            Statement stat = connect.createStatement();
+            String sql = "INSERT INTO users(name,email,phone,address,password)" + "VALUES (?,?,?,?,?)";
+            PreparedStatement preparedStatement=connect.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3,phone);
+            preparedStatement.setString(4,address);
+            preparedStatement.setString(5,password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public static void main(String[] args) {
-                RegistrationForm myReg=new RegistrationForm(null);
+        RegistrationForm myReg = new RegistrationForm(null);
 
     }
 }
