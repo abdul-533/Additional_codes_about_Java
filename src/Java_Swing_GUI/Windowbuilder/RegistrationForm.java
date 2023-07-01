7a9d891f40c2c59ba9fe3af9cfc76f24814b90ce
Abdul <elizadeh9797@gmail.com>
2023-07-01 23:53:24 +0400

    second steps about registration button

--- src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
+++ src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
@@ -1,4 +1,14 @@
 package Java_Swing_GUI.Windowbuilder;
 
+import javax.swing.*;
+
 public class RegistrationForm {
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField tfAddress;
+    private JPasswordField pfPAssword;
+    private JPasswordField pfPasswordConfirm;
+    private JButton btnRegister;
+    private JButton btnCancel;
 }
