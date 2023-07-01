e3ba137c96d277d200a1d0548c0bd52691d7479f
Abdul <elizadeh9797@gmail.com>
2023-07-02 00:25:53 +0400

    next commit about registration

--- src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
+++ src/Java_Swing_GUI/Windowbuilder/RegistrationForm.java
@@ -1,8 +1,9 @@
 package Java_Swing_GUI.Windowbuilder;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class RegistrationForm {
+public class RegistrationForm extends JDialog {
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfPhone;
@@ -11,4 +12,23 @@ public class RegistrationForm {
     private JPasswordField pfPasswordConfirm;
     private JButton btnRegister;
     private JButton btnCancel;
+    private JLabel registerPanel;
+    private JPanel MainPanel;
+
+
+    public RegistrationForm(JFrame parent) {
+        super(parent);
+        setTitle("Create new account");
+        setContentPane(MainPanel);
+        setMinimumSize(new Dimension(700, 720));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+                RegistrationForm myReg=new RegistrationForm(null);
+
+    }
 }
